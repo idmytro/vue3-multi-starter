c6119de8bce9beeb980bc2b9269c4857b9ff6791
Dmytro <dmytro.cisar@gmail.com>
2023-04-16 12:28:55 +0300

    feat: hooks done

--- hooks/src/components/HelloWorld.vue
+++ hooks/src/components/HelloWorld.vue
@@ -1,18 +1,18 @@
 <script setup>
-import { ref } from 'vue'
+import { useState } from 'vue-hooks-api';
 
 defineProps({
-  msg: String,
-})
+	msg: String,
+});
 
-const count = ref(0)
+const [count, setCount] = useState(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="setCount(count++)">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
