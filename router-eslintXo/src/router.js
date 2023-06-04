092014f9d1e3a25a75d3ab66db69924dc414eba5
Dmytro <dmytro.cisar@gmail.com>
2023-06-04 12:50:24 +0300

    build: router-eslintXo/.eslintrc.cjs

--- router-eslintXo/src/router.js
+++ router-eslintXo/src/router.js
@@ -1,27 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router';
 import Home from './views/Home.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import('./views/About.vue'),
-  },
-  {
-    path: '/:path(.*)',
-    name: 'NotFound',
-    component: () => import('./views/NotFound.vue'),
-  },
+	{
+		path: '/',
+		name: 'home',
+		component: Home,
+	},
+	{
+		path: '/about',
+		name: 'about',
+		component: () => import('./views/About.vue'),
+	},
+	{
+		path: '/:path(.*)',
+		name: 'NotFound',
+		component: () => import('./views/NotFound.vue'),
+	},
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
+	history: createWebHistory(),
+	routes,
+});
 
-export default router
+export default router;
