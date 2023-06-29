0d88c7a3257bcf71090f6227f97aba25d958864c
Dmytro <1106994+idmytro@users.noreply.github.com>
2023-06-29 23:58:09 +0300

    Update vite.config.js

--- router-unocss-eslintAntfu/vite.config.js
+++ router-unocss-eslintAntfu/vite.config.js
@@ -1,10 +1,10 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
-import unoCSS from 'unocss/vite';
+import unocss from 'unocss/vite';
 
 export default defineConfig({
 	plugins: [
 		vue(),
-		unoCSS(),
+		unocss(),
 	],
 });
