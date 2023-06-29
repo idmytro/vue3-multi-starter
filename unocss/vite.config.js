e1b95e8d9222ddd799a9fc819d36876ce53e3e0f
Dmytro <1106994+idmytro@users.noreply.github.com>
2023-06-29 23:58:35 +0300

    Update vite.config.js

--- unocss/vite.config.js
+++ unocss/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import UnoCSS from 'unocss/vite'
+import unocss from 'unocss/vite'
 
 export default defineConfig({
   plugins: [
     vue(),
-    UnoCSS({ /* options */ }),
+    unocss(),
   ],
 })
