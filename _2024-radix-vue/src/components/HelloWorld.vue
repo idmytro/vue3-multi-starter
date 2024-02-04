89ce9f1ac525bc06ef44ed0e1461c51d8165d280
Dmytro <dmytro.cisar@gmail.com>
2024-02-04 13:57:54 +0200

    radix done

--- _2024-radix-vue/src/components/HelloWorld.vue
+++ _2024-radix-vue/src/components/HelloWorld.vue
@@ -7,6 +7,40 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
+  <div :style="{ display: 'flex', gap: '20px', justifyContent: 'center' }">
+    <AvatarRoot class="AvatarRoot">
+      <AvatarImage
+        class="AvatarImage"
+        src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+        alt="Colm Tuite"
+      />
+      <AvatarFallback
+        class="AvatarFallback"
+        :delay-ms="600"
+      >
+        CT
+      </AvatarFallback>
+    </AvatarRoot>
+    <AvatarRoot class="AvatarRoot">
+      <AvatarImage
+        class="AvatarImage"
+        src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80"
+        alt="Pedro Duarte"
+      />
+      <AvatarFallback
+        class="AvatarFallback"
+        :delay-ms="600"
+      >
+        JD
+      </AvatarFallback>
+    </AvatarRoot>
+    <AvatarRoot class="AvatarRoot">
+      <AvatarFallback class="AvatarFallback">
+        PD
+      </AvatarFallback>
+    </AvatarRoot>
+  </div>
+
   <div class="card">
     <button type="button" @click="count++">
       count is {{ count }}
@@ -31,8 +65,44 @@ const count = ref(0)
   </p>
 </template>
 
-<style scoped>
+<style>
+@import '@radix-ui/colors/black-alpha.css';
+@import '@radix-ui/colors/grass.css';
+
 .read-the-docs {
   color: #888;
+}
+
+.AvatarRoot {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+  overflow: hidden;
+  user-select: none;
+  width: 45px;
+  height: 45px;
+  border-radius: 100%;
+  background-color: var(--black-a3);
+}
+
+.AvatarImage {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: inherit;
+}
+
+.AvatarFallback {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  color: var(--grass-11);
+  font-size: 15px;
+  line-height: 1;
+  font-weight: 500;
 }
 </style>
--- _2024-radix-vue/vite.config.ts
+++ _2024-radix-vue/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import Component from 'unplugin-vue-components/vite'
+
+// @ts-expect-error import works if radix-vue installed
+import RadixVueResolver from 'radix-vue/resolver'
 
 export default defineConfig({
   plugins: [
@@ -10,6 +14,16 @@ export default defineConfig({
         'vue',
       ],
       dts: './src/auto-imports.d.ts',
+    }),
+    Component({
+      dts: true,
+      resolvers: [
+        RadixVueResolver(),
+
+        // RadixVueResolver({
+        //   prefix: '' // use the prefix option to add Prefix to the imported components
+        // })
+      ],
     }),
   ],
 })
