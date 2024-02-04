bc5fe2f1848b5c64a175b78d4d1747c5d0a5e3c9
Dmytro <dmytro.cisar@gmail.com>
2024-02-04 15:09:20 +0200

    primevue

--- _2024-primevue-uno/src/main.ts
+++ _2024-primevue-uno/src/main.ts
@@ -2,10 +2,14 @@ import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
 import App from './App.vue'
 
+// import '@unocss/reset/tailwind-compat.css' // Reset breaks components, like button
 import './style.css'
 import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'virtual:uno.css'
 
 const app = createApp(App)
-app.use(PrimeVue)
+app.use(PrimeVue, {
+//   unstyled: true,
+})
 
 app.mount('#app')
--- _2024-primevue-uno/vite.config.ts
+++ _2024-primevue-uno/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
   plugins: [
     vue(),
+    UnoCSS(),
     AutoImport({
       imports: [
         'vue',
