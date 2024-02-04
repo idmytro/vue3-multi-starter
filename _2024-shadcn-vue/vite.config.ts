9c9184679c8f0e18ae67bf7efc01264575abc4a8
Dmytro <dmytro.cisar@gmail.com>
2024-02-04 12:33:55 +0200

    rm uno

--- _2024-shadcn-vue/vite.config.ts
+++ _2024-shadcn-vue/vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
@@ -9,7 +8,6 @@ import autoprefixer from 'autoprefixer'
 export default defineConfig({
   plugins: [
     vue(),
-    // UnoCSS(),
     AutoImport({
       imports: [
         'vue',
