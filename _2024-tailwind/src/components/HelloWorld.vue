cb4100659f8684e1bf0bc5717b7f979e8dd887c7
Dmytro <dmytro.cisar@gmail.com>
2024-02-04 13:08:04 +0200

    tailwind done

--- _2024-tailwind/src/components/HelloWorld.vue
+++ _2024-tailwind/src/components/HelloWorld.vue
@@ -5,7 +5,9 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="italic">
+    {{ msg }}
+  </h1>
 
   <div class="card">
     <button type="button" @click="count++">
--- _2024-tailwind/vite.config.ts
+++ _2024-tailwind/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
   plugins: [
     vue(),
-    UnoCSS(),
     AutoImport({
       imports: [
         'vue',
