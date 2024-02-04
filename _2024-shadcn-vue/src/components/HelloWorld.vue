ec6efee5fa0f2f150fb1722d92af4f1f3c5ed385
Dmytro <dmytro.cisar@gmail.com>
2024-02-04 12:21:07 +0200

    done

--- _2024-shadcn-vue/src/components/HelloWorld.vue
+++ _2024-shadcn-vue/src/components/HelloWorld.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
+
 defineProps<{ msg: string }>()
 
 const count = ref(0)
@@ -8,9 +10,11 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">
+    <Button
+      @click="count++"
+    >
       count is {{ count }}
-    </button>
+    </Button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
--- _2024-shadcn-vue/src/main.ts
+++ _2024-shadcn-vue/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // import '@unocss/reset/tailwind-compat.css'
+import './shadcn.css'
 import './style.css'
 // import 'virtual:uno.css'
 
