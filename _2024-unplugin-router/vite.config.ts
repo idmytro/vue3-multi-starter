1e08938c1be51e60107868fec300645f9c403a6f
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2024-06-06 18:44:53 +0200

    types

--- _2024-unplugin-router/vite.config.ts
+++ _2024-unplugin-router/vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import VueRouter from 'unplugin-vue-router/vite'
 
 export default defineConfig({
   plugins: [
+    VueRouter({
+      /* options */
+    }),
     vue(),
     UnoCSS(),
     AutoImport({
