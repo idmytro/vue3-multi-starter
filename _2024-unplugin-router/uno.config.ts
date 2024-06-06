6a6d3348a20242c8b7768881c5782820f31932ce
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2024-06-06 18:42:30 +0200

    2024-unplugin-router start

---
+++ _2024-unplugin-router/uno.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'unocss'
+import transformerDirectives from '@unocss/transformer-directives'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
+
+export default defineConfig({
+  transformers: [
+    transformerDirectives(),
+    // @ts-expect-error transformerVariantGroup is not yet typed
+    transformerVariantGroup(),
+  ],
+})
